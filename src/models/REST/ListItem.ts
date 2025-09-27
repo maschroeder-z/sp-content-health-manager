cc02ff21dc88e4385bd8be2e0b56f868acfdc47d
Marc André Schröder-Zhou <M_A_Zhou@hotmail.com>
2025-09-27 19:06:23 +0200

    Added interface for listitem

---
+++ src/models/REST/ListItem.ts
@@ -0,0 +1,292 @@
+export interface ListItem {
+    __metadata: Metadata
+    FirstUniqueAncestorSecurableObject: FirstUniqueAncestorSecurableObject
+    RoleAssignments: RoleAssignments
+    AttachmentFiles: AttachmentFiles
+    ContentType: ContentType
+    GetDlpPolicyTip: GetDlpPolicyTip
+    FieldValuesAsHtml: FieldValuesAsHtml
+    FieldValuesAsText: FieldValuesAsText
+    FieldValuesForEdit: FieldValuesForEdit
+    File: File
+    Folder: Folder
+    LikedByInformation: LikedByInformation
+    ParentList: ParentList
+    Properties: Properties2
+    Versions: Versions2
+    FileSystemObjectType: number
+    Id: number
+    ServerRedirectedEmbedUri: string
+    ServerRedirectedEmbedUrl: string
+    ContentTypeId: string
+    OData__ColorTag: any
+    ComplianceAssetId: any
+    Title: any
+    Antragsart: string
+    Gesch_x00e4_ftsfeld: any
+    Tags: any
+    Description: any
+    Categories: any
+    ID: number
+    Created: string
+    AuthorId: number
+    Modified: string
+    EditorId: number
+    OData__CopySource: any
+    CheckoutUserId: any
+    OData__UIVersionString: string
+    GUID: string
+  }
+  
+  export interface Metadata {
+    id: string
+    uri: string
+    etag: string
+    type: string
+  }
+  
+  export interface FirstUniqueAncestorSecurableObject {
+    __deferred: Deferred
+  }
+  
+  export interface Deferred {
+    uri: string
+  }
+  
+  export interface RoleAssignments {
+    __deferred: Deferred2
+  }
+  
+  export interface Deferred2 {
+    uri: string
+  }
+  
+  export interface AttachmentFiles {
+    __deferred: Deferred3
+  }
+  
+  export interface Deferred3 {
+    uri: string
+  }
+  
+  export interface ContentType {
+    __deferred: Deferred4
+  }
+  
+  export interface Deferred4 {
+    uri: string
+  }
+  
+  export interface GetDlpPolicyTip {
+    __deferred: Deferred5
+  }
+  
+  export interface Deferred5 {
+    uri: string
+  }
+  
+  export interface FieldValuesAsHtml {
+    __deferred: Deferred6
+  }
+  
+  export interface Deferred6 {
+    uri: string
+  }
+  
+  export interface FieldValuesAsText {
+    __deferred: Deferred7
+  }
+  
+  export interface Deferred7 {
+    uri: string
+  }
+  
+  export interface FieldValuesForEdit {
+    __deferred: Deferred8
+  }
+  
+  export interface Deferred8 {
+    uri: string
+  }
+  
+  export interface File {
+    __metadata: Metadata2
+    Author: Author
+    CheckedOutByUser: CheckedOutByUser
+    EffectiveInformationRightsManagementSettings: EffectiveInformationRightsManagementSettings
+    InformationRightsManagementSettings: InformationRightsManagementSettings
+    ListItemAllFields: ListItemAllFields
+    LockedByUser: LockedByUser
+    ModifiedBy: ModifiedBy
+    Properties: Properties
+    VersionEvents: VersionEvents
+    VersionExpirationReport: VersionExpirationReport
+    Versions: Versions
+    Web: Web
+    CheckInComment: string
+    CheckOutType: number
+    ContentTag: string
+    CustomizedPageStatus: number
+    ETag: string
+    Exists: boolean
+    ExistsAllowThrowForPolicyFailures: boolean
+    ExistsWithException: boolean
+    IrmEnabled: boolean
+    Length: string
+    Level: number
+    LinkingUri: any
+    LinkingUrl: string
+    MajorVersion: number
+    MinorVersion: number
+    Name: string
+    ServerRelativeUrl: string
+    TimeCreated: string
+    TimeLastModified: string
+    Title: any
+    UIVersion: number
+    UIVersionLabel: string
+    UniqueId: string
+  }
+  
+  export interface Metadata2 {
+    id: string
+    uri: string
+    type: string
+  }
+  
+  export interface Author {
+    __deferred: Deferred9
+  }
+  
+  export interface Deferred9 {
+    uri: string
+  }
+  
+  export interface CheckedOutByUser {
+    __deferred: Deferred10
+  }
+  
+  export interface Deferred10 {
+    uri: string
+  }
+  
+  export interface EffectiveInformationRightsManagementSettings {
+    __deferred: Deferred11
+  }
+  
+  export interface Deferred11 {
+    uri: string
+  }
+  
+  export interface InformationRightsManagementSettings {
+    __deferred: Deferred12
+  }
+  
+  export interface Deferred12 {
+    uri: string
+  }
+  
+  export interface ListItemAllFields {
+    __deferred: Deferred13
+  }
+  
+  export interface Deferred13 {
+    uri: string
+  }
+  
+  export interface LockedByUser {
+    __deferred: Deferred14
+  }
+  
+  export interface Deferred14 {
+    uri: string
+  }
+  
+  export interface ModifiedBy {
+    __deferred: Deferred15
+  }
+  
+  export interface Deferred15 {
+    uri: string
+  }
+  
+  export interface Properties {
+    __deferred: Deferred16
+  }
+  
+  export interface Deferred16 {
+    uri: string
+  }
+  
+  export interface VersionEvents {
+    __deferred: Deferred17
+  }
+  
+  export interface Deferred17 {
+    uri: string
+  }
+  
+  export interface VersionExpirationReport {
+    __deferred: Deferred18
+  }
+  
+  export interface Deferred18 {
+    uri: string
+  }
+  
+  export interface Versions {
+    __deferred: Deferred19
+  }
+  
+  export interface Deferred19 {
+    uri: string
+  }
+  
+  export interface Web {
+    __deferred: Deferred20
+  }
+  
+  export interface Deferred20 {
+    uri: string
+  }
+  
+  export interface Folder {
+    __deferred: Deferred21
+  }
+  
+  export interface Deferred21 {
+    uri: string
+  }
+  
+  export interface LikedByInformation {
+    __deferred: Deferred22
+  }
+  
+  export interface Deferred22 {
+    uri: string
+  }
+  
+  export interface ParentList {
+    __deferred: Deferred23
+  }
+  
+  export interface Deferred23 {
+    uri: string
+  }
+  
+  export interface Properties2 {
+    __deferred: Deferred24
+  }
+  
+  export interface Deferred24 {
+    uri: string
+  }
+  
+  export interface Versions2 {
+    __deferred: Deferred25
+  }
+  
+  export interface Deferred25 {
+    uri: string
+  }
+  
